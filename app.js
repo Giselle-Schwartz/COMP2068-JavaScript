816a1ed75d5cfa844b3440e3dc150326b450631c
Giselle-Schwartz <giselleschwartz@gmail.com>
2019-05-23 16:50:55 -0400

    Dynamic port assignment

--- app.js
+++ app.js
@@ -10,4 +10,6 @@ app.get('/about', (req, res) => {
     res.send("Yooooooooooooo");
 });
 
-app.listen(3000, () => console.log("Listening on port 3000"));
+const port = (process.env.PORT || 4000)
+
+app.listen(port, () => console.log(`Listening on port ${port}`));
